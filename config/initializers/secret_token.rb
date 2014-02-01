42278ac38191853e47dd2f15966064bb563470c1
Phuong H. Le <leph_00@yahoo.com>
2014-01-31 20:21:51 -0500

    Initialize repository for Team Alpha - IT499

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamAlpha::Application.config.secret_key_base = 'a9122a051e877e674591e5a92cebb4a6455d0bd69f9b2c4f5dc5464a359999bf80a7b6917a4854c4397c4cfe4254ca4667e73cf071653a98a31da967d23b27e7'
