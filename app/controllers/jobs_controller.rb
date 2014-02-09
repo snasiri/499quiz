058699f3ef7549ddd77b122ce795cc540a0c4dff
Phuong H. Le <leph_00@yahoo.com>
2014-02-09 09:44:05 -0500

    Updated job resource files to show new column, probably was a better way.. ~leph

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -69,6 +69,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:title, :location)
+      params.require(:job).permit(:title, :user_id, :location)
     end
 end
