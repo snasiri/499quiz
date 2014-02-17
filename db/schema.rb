fc643054b6b3585b089b185aa56a1725b849f82b
Phuong H. Le <leph_00@yahoo.com>
2014-02-17 07:37:22 -0500

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140216025954) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "certs", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
@@ -54,9 +51,8 @@ ActiveRecord::Schema.define(version: 20140216025954) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
