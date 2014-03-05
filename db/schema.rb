8aaf02d076afc8cacdc8bf4950607a5b365c59c4
snasiri <snasiri1@Alladins-MacBook-Air.local>
2014-03-04 19:21:09 -0500

    generated new migrations to repaird db.  ~shaun

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225232509) do
+ActiveRecord::Schema.define(version: 20140305001713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "certs", force: true do |t|
     t.string   "title"
